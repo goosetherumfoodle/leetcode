35d0315958baa37f073e18f4b4b35aa90f9675c7
goosetherumfoodle <13107006+goosetherumfoodle@users.noreply.github.com>
2022-10-15 13:19:46 -0400

    problem 189

--- src/lib.rs
+++ src/lib.rs
@@ -483,3 +483,52 @@ mod problem_53 {
         }
     }
 }
+
+mod problem_189 {
+    /*
+    Rotate Array
+Medium
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+*/
+    pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+
+    }
+
+
+     #[cfg(test)]
+    mod tests {
+        fn example_1() {
+        /*
+        Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+            let mut nums = [1,2,3,4,5,6,7];
+            let k = 3;
+
+            rotate(nums, k);
+
+            assert_eq!(nums, vec![5,6,7,1,2,3,4]);
+        }
+
+        fn example_2() {
+/*
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+            let mut nums = [-1,-100,3,99];
+            let k = 2;
+
+            rotate(nums, k);
+
+            assert_eq!(nums, vec![3,99,-1,-100]);
+        }
+    }
+}
